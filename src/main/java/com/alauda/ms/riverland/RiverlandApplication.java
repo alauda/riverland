c21a30c6f2fd4d2ec9ae8f76774a101a26bc2abe
ZhouLei <leizhou@alauda.io>
2021-02-10 15:04:05 +0800

    Added Spring Cloud Kubernetes dependencies.

--- src/main/java/com/alauda/ms/riverland/RiverlandApplication.java
+++ src/main/java/com/alauda/ms/riverland/RiverlandApplication.java
@@ -2,8 +2,10 @@ package com.alauda.ms.riverland;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RiverlandApplication {
 
 	public static void main(String[] args) {
